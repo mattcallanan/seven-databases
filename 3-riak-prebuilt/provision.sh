bbf0718ceb6ea57de70ed1fd68340f263f7201c9
mattcallanan <matthew_callanan@yahoo.com.au>
2012-10-02 09:44:30 +1000

    Fixes for HBase setup

--- 3-riak-prebuilt/provision.sh
+++
@@ -1,7 +0,0 @@
-apt-get -q update
-apt-get -y install libssl0.9.8 git curl
-cd /opt
-tar zxvf /vagrant/riak-1.2.0-dev.tar.gz
-chown -hR vagrant.vagrant riak-1.2.0 
-cp /vagrant/.bashrc ~vagrant/
-exit
